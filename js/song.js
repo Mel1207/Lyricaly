b0a34f686d69039bea8a07927d55fedf5124ff4e
Gerard Balaoro <gmbalaoro@outlook.com>
2019-12-10 20:17:01 +0800

    Added search method in song.js

--- js/song.js
+++ js/song.js
@@ -35,6 +35,51 @@ export class Song
     static async search(keyword)
     {
         // Search songs from iTunes API
+        // Create unique request key, required to bypass CORS/CORB error
+		const key = (Math.random() + Date.now()).toString(36).substring(2);
+
+		// Create search URL string
+		const url = `https://itunes.apple.com/search?media=music&entity=song&term=${keyword}&${key}`;
+
+		// Intialize results array
+		let results = [];
+
+		// Create HTTP request using `fetch`
+		const request = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Accept': 'application/json',
+				'Authorization': 'Bearer ' + key,
+				'Content-Type': 'application/json'
+			},
+		})
+
+		// Read the response as JSON and convert it to Object
+		.then(response => {
+			return response.json()
+		})
+
+		// Do stuff with the data
+		.then(response => {
+			response.results.forEach((result) => {
+				// Check if result item has `kind` property of 'song'
+				if (result.kind === 'song') {
+
+					// Convert each item to a new `Song` object
+					results.push(new Song(
+						result.trackId, // id
+						result.trackName, // title
+						result.artistName, // artist						
+                        result.collectionName, // album,
+                        (new Date(result.releaseDate)).getFullYear(), // year
+						result.artworkUrl100 // cover
+					));
+				}
+			});
+		});
+
+		return results;			
     }
 
     inFavorites()
