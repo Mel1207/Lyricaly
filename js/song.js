48064f58ab4fda86ec5a25a57c4b1e55d595aa80
Gerard Balaoro <gmbalaoro@outlook.com>
2019-12-11 11:14:59 +0800

    Add comments on song.js

--- js/song.js
+++ js/song.js
@@ -75,22 +75,41 @@ export class Song
         return new Song(song.id, song.title, song.artist, song.album, song.year, song.cover);
     }
 
+	/**
+	 * Removes all favorites from `localStorage`
+	 * 
+	 * @memberof Song
+	*/
     static clearFavorites()
     {
         // Empty favorites in localStorage
         localStorage.setItem('songs', JSON.stringify([]));
     }
 
+	/**
+	 * Searches songs using iTunes API
+	 *
+	 * @param keyword
+	 * @returns {array}
+	 * @memberof Song
+	*/
     static async search(keyword)
     {
-        // Search songs from iTunes API
-        // Create unique request key, required to bypass CORS/CORB error
+        /**
+		 * Create unique request key, required to bypass CORS/CORB error.
+		 * This is a dirty hack so iTunes won't block if we send to much
+		 * AJAX requests in a short period of time.
+		 *
+		 * What this is doing is creating a unique string that we append
+		 * to our fetch URL. It's okay if you don't understand this.
+		*/
 		const key = (Math.random() + Date.now()).toString(36).substring(2);
 
-		// Create search URL string
+		// Create search URL string (Note that we appended the key)
 		const url = `https://itunes.apple.com/search?media=music&entity=song&term=${keyword}&limit=10&country=PH&${key}`;
 
 		// Intialize `results` array
+		// So we can return an empty array, if AJAX fails
 		let results = [];
 
 		// Create AJAX request using `fetch`
@@ -98,11 +117,11 @@ export class Song
             // Read response as json
             return response.json()
         })
-
-        // Loop through each response object
         .then(response => {
+        	// Loop through each response object
             response.results.forEach((result) => {
                 // Check if result item has `kind` property of 'song'
+                // Because sometimes iTunes will give us an audiobook (weird)
                 if (result.kind === 'song') {
 
                     // Convert each item to a new `Song` instance then append it to `results`
@@ -121,28 +140,64 @@ export class Song
 		return results;			
     }
 
+	/**
+	 * Checks if song is in favorites
+	 *
+	 * @returns {boolean}
+	 * @memberof Song
+	*/
     inFavorites()
     {
-        // Check if song is in favorites
+    	// Find the song and check if its not undefined
         return Song.getFavorite(this.id) !== undefined;
     }
 
+	/**
+	 * Adds song to favorites
+	 * 
+	 * @memberof Song
+	*/
     addToFavorites()
     {
-        // Add song to favorites in localStorage
+        // First we check if song is already in favorites
+        if (this.inFavorites()) {
+        	// Stop the function, there's nothing to do here
+        	return;
+        }
+        
+        // Get all songs from `localStorage`
         let songs = Song.allFavorites();
+        
+        // Append current song
         songs.push(this);
+        
+        // Put the new songs array in `localStorage`
         localStorage.setItem('songs', JSON.stringify(songs));
     }
 
-
+	/**
+	 * Removes song from favorites
+	 *
+	 * @memberof Song
+	*/
     removeFromFavorites()
     {
-        // Remove song from favorites in localStorage
+    	// Check if song is not in favorites
+    	if (!this.inFavorites()) {
+    		// Stop function, there's nothing to do here
+    		return;
+    	}
+    
+        // Get all songs from `localStorage`
         let songs = Song.allFavorites();
+        
+        // Filter through each song
         songs = songs.filter((song) => {
+        	// Remove the ones that matches the `id` of the song we want to remove
             return song.id != this.id;
         });
+        
+        // Put the new songs array in `localStorage`
         localStorage.setItem('songs', JSON.stringify(songs));
     }
 }
